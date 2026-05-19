ad945b270c5b4f95b8bdffbe3de9e86e0de56bae
Rustam Sadreev <rustam17.sb@gmail.com>
2026-05-19 19:31:31 +0300

    lesson-3

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,23 +1,66 @@
 function getGoods() {
   const linkElements = document.querySelectorAll('.navigation-link');
 
-  function getData() {
+  function renderGoods(goods) {
+    const goodListElement = document.querySelector('.long-goods-list');
+    goodListElement.innerHTML = '';
+
+    goods.forEach((good) => {
+      const goodListItemElement = document.createElement('div');
+      goodListItemElement.classList.add('col-lg-3');
+      goodListItemElement.classList.add('col-sm-6');
+
+      const { id, name, label, img, description, price } = good;
+
+      goodListItemElement.innerHTML = `
+        <div class="goods-card">
+          <span class="label ${label ? null : 'd-none'}">${label}</span>
+          <img src="db/${img}" alt="image: ${name}" class="goods-image">
+          <h3 class="goods-title">${name}</h3>
+          <p class="goods-description">${description}</p>
+          <button class="button goods-card-btn add-to-cart" data-id="${id}">
+            <span class="button-price">$${price}</span>
+          </button>
+        </div>
+      `;
+
+      goodListElement.append(goodListItemElement)
+    });
+  }
+
+  function getData(filterProperty, value) {
     fetch('https://willberries-c4ae1-default-rtdb.firebaseio.com/db.json')
       .then((response) => response.json())
       .then((data) => {
-        localStorage.setItem('goods', JSON.stringify(data))
+        const filteredGoods = filterProperty
+          ? data.filter((good) => good[filterProperty] === value)
+          : data;
+
+        localStorage.setItem('goods', JSON.stringify(filteredGoods))
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(filteredGoods);
+        }
       });
   }
 
   linkElements.forEach((linkElement) => {
     linkElement.addEventListener('click', (evt) => {
       evt.preventDefault();
-      getData();
+      getData(linkElement.dataset.field, linkElement.textContent);
     });
   });
 
-  const goods = JSON.parse(localStorage.getItem('goods'))
-  console.log(goods);
+  document.querySelector('a.more')?.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    getData();
+  });
+
+  if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('goods')));
+  }
 }
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -2,7 +2,52 @@
   const searchInputElement = document.querySelector('.search-block input');
   const searchButtonElement = document.querySelector('.search-block button');
 
+  function renderGoods(goods) {
+    const goodListElement = document.querySelector('.long-goods-list');
+    goodListElement.innerHTML = '';
+
+    goods.forEach((good) => {
+      const goodListItemElement = document.createElement('div');
+      goodListItemElement.classList.add('col-lg-3');
+      goodListItemElement.classList.add('col-sm-6');
+
+      const { id, name, label, img, description, price } = good;
+
+      goodListItemElement.innerHTML = `
+        <div class="goods-card">
+          <span class="label ${label ? null : 'd-none'}">${label}</span>
+          <img src="db/${img}" alt="image: ${name}" class="goods-image">
+          <h3 class="goods-title">${name}</h3>
+          <p class="goods-description">${description}</p>
+          <button class="button goods-card-btn add-to-cart" data-id="${id}">
+            <span class="button-price">$${price}</span>
+          </button>
+        </div>
+      `;
+
+      goodListElement.append(goodListItemElement)
+    });
+  }
+
+  function getData(searchQuery) {
+    fetch('https://willberries-c4ae1-default-rtdb.firebaseio.com/db.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const foundGoods = data.filter((good) =>
+          good.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        localStorage.setItem('goods', JSON.stringify(foundGoods))
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(foundGoods);
+        }
+      });
+  }
+
   searchButtonElement.addEventListener('click', () => {
-    console.log(searchInputElement.value);
+    getData(searchInputElement.value);
   });
 })();
